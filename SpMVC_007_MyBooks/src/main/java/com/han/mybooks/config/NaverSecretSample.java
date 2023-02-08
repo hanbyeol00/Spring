c25ab0b3569f0476c35a899ae0394d8de91b541d
hanbyeol00 <gksquf6670@gmail.com>
2023-02-08 11:19:45 +0900

    naver

---
+++ SpMVC_007_MyBooks/src/main/java/com/han/mybooks/config/NaverSecretSample.java
@@ -0,0 +1,14 @@
+package com.han.mybooks.config;
+
+/**
+ * Naver 도서검색을 위하여
+ * 자신의 Application CLAIENT_ID, CLAIENT_SECRET 를 아래 변수에
+ * 추가한 후 클래스 이름을 NaverSecret 로 변경한 후 프로젝트를 Run 하시오
+ */
+public class NaverSecretSample {
+	
+	public final static String CLAIENT_ID="";
+	public final static String CLAIENT_SECRET="";
+	public final static String NAVER_BOOK_URL_JSON="https://openapi.naver.com/v1/search/book.json";
+	
+}
